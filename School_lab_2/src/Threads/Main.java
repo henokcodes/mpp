eb1116d36ed074169dd0cff7bca9fadfd5f35801
henok <henokcodes@gmail.com>
2023-01-26 10:41:59 -0600

    Prime Checker program using Threads finish ups.

--- School_lab_2/src/Threads/Main.java
+++ School_lab_2/src/Threads/Main.java
@@ -2,4 +2,20 @@ package Threads;
 
 public class Main {
 
+
+
+    public static void main(String[] args) {
+        PrimeChecker object1 = new PrimeChecker(1,20);
+        PrimeChecker object2 = new PrimeChecker(21,40);
+        PrimeChecker object3 = new PrimeChecker(41,60);
+        PrimeChecker objec4 = new PrimeChecker(61,80);
+
+        new Thread(object1).start();
+        new Thread(object2).start();
+        new Thread(object3).start();
+        new Thread(objec4).start();
+
+
+
+    }
 }
--- School_lab_2/src/Threads/PrimeChecker.java
+++ School_lab_2/src/Threads/PrimeChecker.java
@@ -1,4 +1,31 @@
 package Threads;
 
-public class PrimeChecker {
+public class PrimeChecker implements Runnable{
+    private int lowerLimit;
+    private int upperLimit;
+
+    public PrimeChecker(int lowerLimit, int upperLimit) {
+        this.lowerLimit = lowerLimit;
+        this.upperLimit = upperLimit;
+    }
+
+    boolean isPrime(int n){
+        if (n <= 1)
+            return true;
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    @Override
+    public void run() {
+        for (int i=lowerLimit;i<=upperLimit;i++){
+            if(isPrime(i)){
+                System.out.println(i+ " ");
+            }
+        }
+    }
 }
+
