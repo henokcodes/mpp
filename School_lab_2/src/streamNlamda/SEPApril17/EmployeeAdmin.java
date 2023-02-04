5fd07b4a3b63f13cf3cb8b146f2a7224050a68b4
henok <henokcodes@gmail.com>
2023-02-04 13:27:40 -0600

    stream

--- School_lab_2/src/streamNlamda/SEPApril17/EmployeeAdmin.java
+++ School_lab_2/src/streamNlamda/SEPApril17/EmployeeAdmin.java
@@ -2,21 +2,29 @@ package streamNlamda.SEPApril17;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeAdmin {
 
-    // returns a list of social security numbers, sorted in ascending order,
+    //q: returns a list of social security numbers, sorted in ascending order,
     // that belong to an Employee in the input table but that are not on the
     // socSecNums input list
+
     public static List<String> prepareSsnReport(HashMap<String, Employee> table, List<String> socSecNums) {
         //implement
-        return null;
-
+        return socSecNums.stream()
+//                .filter(s -> (table.containsKey(s)))
+                .sorted().collect(Collectors.toList());
     }
     //return a list of Employees whose social security number is on the input list socSecNums
     //and whose salary is >80000; the return list does not need to be sorted
     public static List<Employee> prepareEmpReport(HashMap<String, Employee> table, List<String> socSecNums) {
         //implement
-        return null;
+        return table.values().stream()
+                .filter( s -> table.containsValue(s))
+                .filter(e-> e.getSalary()>80000)
+                .collect(Collectors.toList());
+
+
     }
 }
